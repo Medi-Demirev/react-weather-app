8d210111842080dc8f3aa4ca36ee5af09ee75ba9
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-12-31 13:07:13 +0200

    Create api services

---
+++ src/services/apiServices.js
@@ -0,0 +1,10 @@
+
+export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': '8c35b71116msh9fe1cdafce16089p1bff56jsn499491c22987',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
