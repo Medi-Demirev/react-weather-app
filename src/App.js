12d21790bc8ae78287a0dd8e600d94e1c0480765
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-12-31 19:50:34 +0200

    Implement logic for the current weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import Search from './components/Search/Search';
+import CurrentWeather from './components/CurrentWether/CurrentWeather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './constants/constants';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecst] = useState(null);
+
+  const handleOnSearchChange =(searchData)=>{
+   const [lat, lon] = searchData.value.split(" "); 
+   const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+   const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+  Promise.all([currentWeatherFetch, forecastFetch])
+    .then(async (response) =>{
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+
+      setCurrentWeather({city:searchData.label, ...weatherResponse});
+      setForecst({city:searchData.label, ...forecastResponse});
+
+    })
+    .catch((err)=> console.log(err));
+  }
+  console.log(currentWeather);
+  console.log(forecast);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
+      {currentWeather && <CurrentWeather data={currentWeather}/>}
     </div>
   );
 }
