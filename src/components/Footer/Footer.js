f3d4b4ec0f8ab1e81f18dd88165a68fdb285bbae
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-01 21:50:27 +0200

    Add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="footer">  
+                    <div className="footer-item">
+                        <img className="footer-icon" alt="footer-weather" src="weather-favicon.png"></img>
+                        <p className="copyright"> Weather Application &copy; 2023 </p>
+                    </div>
+    </footer>
+    )
+};
+
+export default Footer;
